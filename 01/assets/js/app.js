58ce2f1b33cb904af2154116aaf77115cef72104
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-03-31 20:37:51 +0200

    pierwszy punkt

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,13 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+// -------------------------- 1. ----------------------------------------------------------------
+
+console.log('Zmienna a jest typu', typeof a);
+console.log('Zmienna b jest typu', typeof b);
+
+// -------------------------- 2. ----------------------------------------------------------------
+
+const newA = parseFloat(a);
+console.log(typeof newA);
