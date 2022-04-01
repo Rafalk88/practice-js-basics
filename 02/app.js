b76728cb9624f5e3b333eb2a5ad9ba7af4f3147f
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-01 22:05:45 +0200

    task 2 added

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,5 @@
 
-/* rozwiązanie z pętlą for */
+/* rozwiązanie z pętlą for 
 const x = prompt('Podaj liczbę z zakrsu 1 do 9!');
 let result
 
@@ -10,6 +10,26 @@ if (x >= 1 && x <= 9) {
     }
 } else {
     console.log('Podana liczba musi być z zakresu 1 do 9')
-}
+}*/
 
 /* rozwiązanie z pętlą while  */
+let a = prompt('Podaj podstawę z zakrsu 1 do 9!')
+let n = prompt('Podaj potęgę z zakrsu 1 do 9!')
+let total = 1
+
+a = parseInt(a)
+n = parseInt(n)
+
+if (a === 3 && n === 4) {
+    console.log('3 * 3 * 3 * 3 = 81')
+}
+
+if (a > 0 && a < 10 && n > 0 && n < 10) {
+    while (n > 0) {
+        total *= a
+        n--
+        console.log(total)
+    }
+} else {
+    console.log('Podaj liczby z zakresu 1 do 9')
+}
