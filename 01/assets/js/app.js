9481928525055f6a523678c0276f1da7b9afbebe
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-01 20:29:53 +0200

    task 1 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -11,4 +11,49 @@ console.log('Zmienna b jest typu', typeof b);
 // -------------------------- 2. ----------------------------------------------------------------
 
 const newA = parseFloat(a);
-console.log(typeof newA);
+
+const newA2 = newA + b;
+
+const newA3 = newA2 - b;
+
+const newA4 = newA3 * b;
+
+const newA5 = newA4 / b;
+
+const newA6 = newA5 % b;
+
+let newB = 9
+newB++;
+
+let newB1 = 9
+newB--;
+
+const newA9 = a + b;
+
+if (newA2 > 20) {
+    console.log('Wynik: ' + newA2 + ' jest większy niż 20')
+} else {
+    console.log('Wynik: ' + newA2 + ' jest mniejszy niż 20')
+}
+
+if (newA3 > 20) {
+    console.log('Wynik: ' + newA3 + ' jest większy niż 20')
+} else {
+    console.log('Wynik: ' + newA3 + ' jest mniejszy niż 20')
+}
+
+if (newA4 > 20) {
+    console.log('Wynik: ' + newA4 + ' jest większy niż 20')
+} else {
+    console.log('Wynik: ' + newA4 + ' jest mniejszy niż 20')
+}
+
+if (newA5 > 20) {
+    console.log('Wynik: ' + newA5 + ' jest większy niż 20')
+} else {
+    console.log('Wynik: ' + newA5 + ' jest mniejszy niż 20')
+}
+
+// -----------------------------------------------------------------------------------------
+
+
