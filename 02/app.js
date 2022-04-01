fb7e9bd7118efbf52b0aef7072202cd13255ba99
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-01 20:40:58 +0200

    general x loop added with if

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,15 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę z zakrsu 1 do 9!');
 let result
 
-for (let i = 1; i < 10; i++) {
-    result = i * x
-    console.log(i + ' x ' + x + ' = ' + result)
+if (x >= 1 && x <= 9) {
+    for (let i = 1; i < 10; i++) {
+        result = i * x
+        console.log(i + ' x ' + x + ' = ' + result)
+    }
+} else {
+    console.log('Podana liczba musi być z zakresu 1 do 9')
 }
-
-
 
 /* rozwiązanie z pętlą while  */
